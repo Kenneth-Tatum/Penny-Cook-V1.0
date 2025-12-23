8431abd7ab1c94b1fee96d2b901eebef865aa6c6
Kenneth Tatum <kennethdtatum@gmail.com>
2025-12-23 01:09:37 -0500

    Started a new header component and updated the main index file to include it.

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import {Header} from "./components/header";
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        {Header()}
+      
+    </View>
+  );
+}
